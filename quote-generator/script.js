9dc6363048f24af6ca3484d0472e66694bf159f1
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-01-21 00:04:45 +0900

    Feat: DOM manipulation

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,14 +1,32 @@
+// HTML 요소 접근
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote-text');
+const authorText = document.getElementById('quote-author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
-// New quote
+// 새로운 명언 함수
 function newQuote() {
-	// Get a random quote
+	// 랜덤 명언 선택
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 	console.log(quote);
-	return quote;
+	if (!quote.author) {
+		quote.author = 'Unknown';
+	}
+	authorText.textContent = quote.author;
+
+	// 명언 텍스트 길이에 따라 스타일링 변경
+	if (quote.text.length > 50) {
+		quoteText.classList.add('long-quote');
+	} else {
+		quoteText.classList.remove('long-quote');
+	}
+	quoteText.textContent = quote.text;
 }
 
-// Get quotes from API
+// API에서 quotes 가져오기
 async function getQuotes() {
 	const apiUrl = 'https://type.fit/api/quotes';
 	try {
@@ -20,5 +38,16 @@ async function getQuotes() {
 	}
 }
 
-// On load
+// 트위터에 명언 남기기
+function tweetQuote() {
+	const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+	// 새로운 탭에서 트위터 url 열기
+	window.open(twitterUrl, '_blank');
+}
+
+// 이벤트 리스너 추가
+twitterBtn.addEventListener('click', tweetQuote);
+newQuoteBtn.addEventListener('click', newQuote);
+
+// 자바스크립트 로드 시 작동
 getQuotes();
