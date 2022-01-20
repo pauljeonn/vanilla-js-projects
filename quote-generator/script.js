c95434df97eecb0dd287ba6fe088de2d648a47fd
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-01-21 00:32:26 +0900

    Feat: Add css loader

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -4,11 +4,25 @@ const quoteText = document.getElementById('quote-text');
 const authorText = document.getElementById('quote-author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// 로딩 함수
+function startLoading() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+}
+
+// 로딩 완료 함수
+function finishLoading() {
+	quoteContainer.hidden = false;
+	loader.hidden = true;
+}
+
 // 새로운 명언 함수
 function newQuote() {
+	startLoading();
 	// 랜덤 명언 선택
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 	console.log(quote);
@@ -23,11 +37,14 @@ function newQuote() {
 	} else {
 		quoteText.classList.remove('long-quote');
 	}
+	// 명언 텍스트 할당 후 로딩 멈추기
 	quoteText.textContent = quote.text;
+	finishLoading();
 }
 
 // API에서 quotes 가져오기
 async function getQuotes() {
+	startLoading();
 	const apiUrl = 'https://type.fit/api/quotes';
 	try {
 		const response = await fetch(apiUrl);
