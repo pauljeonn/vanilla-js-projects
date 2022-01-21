fd4aaa7eb5c918232c540aa68e3eeb3ab7a97058
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-01-21 13:16:55 +0900

    Refactoring: setAttibutes 헬퍼 함수 적용

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -10,18 +10,29 @@ const count = 10;
 const apiKey = '2IUcEYfwmAKWojV64Nra2EPQGAcwoOb33UyZJzgVaSM';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
+// Helper Function for setAttribute
+function setAttributes(element, attributes) {
+	for (const key in attributes) {
+		element.setAttribute(key, attributes[key]);
+	}
+}
+
 // 이미지 및 링크 요소 생성 및 DOM에 추가하기
 function displayPhotos() {
 	photos.forEach((photo) => {
 		// Unsplash 링크 생성
 		const item = document.createElement('a');
-		item.setAttribute('href', photo.links.html);
-		item.setAttribute('target', '_blank');
+		setAttributes(item, {
+			href: photo.links.html,
+			target: '_blank',
+		});
 		// 이미지 생성
 		const img = document.createElement('img');
-		img.setAttribute('src', photo.urls.regular);
-		img.setAttribute('alt', photo.alt_description);
-		img.setAttribute('title', photo.alt_description);
+		setAttributes(img, {
+			src: photo.urls.regular,
+			alt: photo.alt_description,
+			title: photo.alt_description,
+		});
 		// 링크 요소 안에 이미지 넣어주고, imageContainer 안에 링크 아이템 넣기
 		item.appendChild(img);
 		imageContainer.appendChild(item);
