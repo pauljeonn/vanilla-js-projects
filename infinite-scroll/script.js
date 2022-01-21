1bce3307f8c00f6830d9ce3b6d44c39ca12af8c3
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-01-21 14:15:26 +0900

    Feat: 이벤트 리스너 사용하여 무한스크롤 구현

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -5,8 +5,13 @@ const loader = document.getElementById('loader');
 // 사진 정보를 저장하는 photos 변수 생성
 let photos = [];
 
+// 이미지 로딩 관련 변수 생성
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = '2IUcEYfwmAKWojV64Nra2EPQGAcwoOb33UyZJzgVaSM';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
@@ -17,8 +22,22 @@ function setAttributes(element, attributes) {
 	}
 }
 
+// 이미지 1개 로드될때마다 호출
+function imageLoaded() {
+	imagesLoaded++;
+	console.log('images loaded');
+	if (imagesLoaded === totalImages) {
+		ready = true;
+		console.log(ready);
+	}
+}
+
 // 이미지 및 링크 요소 생성 및 DOM에 추가하기
 function displayPhotos() {
+	// totalImages 갯수 설정 및 imagesLoaded 리셋
+	totalImages = photos.length;
+	imagesLoaded = 0;
+
 	photos.forEach((photo) => {
 		// Unsplash 링크 생성
 		const item = document.createElement('a');
@@ -33,6 +52,8 @@ function displayPhotos() {
 			alt: photo.alt_description,
 			title: photo.alt_description,
 		});
+		// 이벤트 리스너 사용해서 이미지 한개씩 로드될때마다 체크
+		img.addEventListener('load', imageLoaded);
 		// 링크 요소 안에 이미지 넣어주고, imageContainer 안에 링크 아이템 넣기
 		item.appendChild(img);
 		imageContainer.appendChild(item);
@@ -50,4 +71,16 @@ async function getPhotos() {
 	}
 }
 
+// 페이지 바닥에 닿을 때 추가 사진 로드
+window.addEventListener('scroll', () => {
+	if (
+		window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+		ready
+	) {
+		ready = false;
+		getPhotos();
+	}
+});
+
+// 자바스크립트 로드 시
 getPhotos();
