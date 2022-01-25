4a5ebbac8249874a3924867d15ce983c25df564a
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-01-25 17:16:04 +0900

    Refactor: 이미지 및 아이콘 핸들링

--- light-dark-mode/script.js
+++ light-dark-mode/script.js
@@ -6,32 +6,33 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+// 이미지 변경 함수
+function imageMode(mode) {
+	image1.src = `img/undraw_proud_coder_${mode}.svg`;
+	image2.src = `img/undraw_feeling_proud_${mode}.svg`;
+	image3.src = `img/undraw_conceptual_idea_${mode}.svg`;
+}
+
 // 다크 모드 스타일
 const darkMode = () => {
 	nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
 	textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
 	// 다크 모드 텍스트 및 아이콘 변경
 	toggleIcon.children[0].textContent = 'Dark Mode';
-	toggleIcon.children[1].classList.remove('fa-sun');
-	toggleIcon.children[1].classList.add('fa-moon');
+	toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
 	// 다크 모드용 이미지로 변경
-	image1.src = 'img/undraw_proud_coder_dark.svg';
-	image2.src = 'img/undraw_feeling_proud_dark.svg';
-	image3.src = 'img/undraw_conceptual_idea_dark.svg';
+	imageMode('dark');
 };
 
 // 라이트 모드 스타일
 const lightMode = () => {
 	nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
 	textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-	// 다크 모드 텍스트 및 아이콘 변경
+	// 라이트 모드 텍스트 및 아이콘 변경
 	toggleIcon.children[0].textContent = 'Light Mode';
-	toggleIcon.children[1].classList.remove('fa-moon');
-	toggleIcon.children[1].classList.add('fa-sun');
-	// 다크 모드용 이미지로 변경
-	image1.src = 'img/undraw_proud_coder_light.svg';
-	image2.src = 'img/undraw_feeling_proud_light.svg';
-	image3.src = 'img/undraw_conceptual_idea_light.svg';
+	toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+	// 라이트 모드용 이미지로 변경
+	imageMode('light');
 };
 
 // 동적으로 테마 변경
