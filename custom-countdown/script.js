b7aab7bbb4291e95201dd3f17d42e2f5fdd31aa8
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-02-06 22:39:39 +0900

    Feat: Form title 및 date 추출

--- custom-countdown/script.js
+++ custom-countdown/script.js
@@ -2,7 +2,26 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+let countdownValue = Date;
+
 // 오늘 날짜로 Min Date 설정하기
 const today = new Date().toISOString().split('T')[0]; // YYYY-MM-DD만 가져오기
 console.log(today);
 dateEl.setAttribute('min', today);
+
+// Form Input 값 가져오기
+function updateCountdown(e) {
+	e.preventDefault();
+	// submit 정보에서 title과 date 값 추출
+	countdownTitle = e.srcElement[0].value;
+	countdownDate = e.srcElement[1].value;
+	console.log(countdownTitle, countdownDate);
+	// 추출한 date 값을 숫자로 변환
+	countdownValue = new Date(countdownDate).getTime();
+	console.log(countdownValue);
+}
+
+// 이벤트 리스너
+countdownForm.addEventListener('submit', updateCountdown);
