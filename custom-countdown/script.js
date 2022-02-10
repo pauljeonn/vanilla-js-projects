e003a500d098a41aef07659825e7faa2d71d0079
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-02-10 19:53:34 +0900

    bookmark-keeper 프로젝트 생성

--- custom-countdown/script.js
+++ custom-countdown/script.js
@@ -13,7 +13,7 @@ const completeBtn = document.getElementById('complete-button');
 
 let countdownTitle = '';
 let countdownDate = '';
-let countdownValue = Date;
+let countdownValue;
 let countdownActive;
 let savedCountdown;
 
