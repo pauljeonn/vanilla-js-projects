affd7ffe017cba2cf6f0921aa12cba1e74443fc7
Paul Jeon <49374104+pauljeonn@users.noreply.github.com>
2022-03-22 23:00:50 +0900

    Feat: 비밀번호 validation

--- form-validation/script.js
+++ form-validation/script.js
@@ -5,14 +5,32 @@ const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
 let isValid = false;
+let passwordMatch = false;
 
 function validateForm() {
 	// Using Constraint API
 	isValid = form.checkValidity();
+
 	// Style main message for an error
-	message.textContent = 'Please fill out all fields.';
-	message.style.color = 'red';
-	messageContainer.style.borderColor = 'red';
+	if (!isValid) {
+		message.textContent = 'Please fill out all fields.';
+		message.style.color = 'red';
+		messageContainer.style.borderColor = 'red';
+	}
+
+	// Check to see if passwords match
+	if (password.value === confirmPassword.value) {
+		passwordMatch = true;
+		password.style.borderColor = 'green';
+		confirmPassword.style.borderColor = 'green';
+	} else {
+		passwordMatch = false;
+		message.textContent = 'Make sure passwords match.';
+		message.style.color = 'red';
+		messageContainer.style.borderColor = 'red';
+		password.style.borderColor = 'red';
+		confirmPassword.style.borderColor = 'red';
+	}
 }
 
 function processFormData(e) {
